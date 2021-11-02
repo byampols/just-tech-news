55d81cecc55a52c68f06f6e345c938cc3d194301
unknown <byampols@alumni.cmu.edu>
2021-11-02 16:58:22 -0400

    added partials and extra formatting

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+const helpers = require('./utils/helpers');
+const hbs = exphbs.create({helpers});
 
 const app = express();
 const PORT = process.env.PORT || 3001;
