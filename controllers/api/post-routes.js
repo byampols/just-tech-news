dcd09de5e5569c99a8065ef328a6bb0ed7d23b96
unknown <byampols@alumni.cmu.edu>
2021-11-02 13:12:29 -0400

    added page for single posts

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -78,10 +78,13 @@ router.post('/', (req, res) => {
 
 //upvote a post
 router.put('/upvote', (req, res) => {
-    Post.upvote(req.body, { Vote }).then(updatedPostData => res.json(updatedPostData)).catch(err => {
-        console.log(err);
-        res.status(400).json(err);
-    });
+    if (req.session) {
+        Post.upvote({...req.body, user_id: req.session.user_id}, { Vote, Comment, User })
+            .then(updatedPostData => res.json(updatedPostData)).catch(err => {
+                console.log(err);
+                res.status(500).json(err);
+            });
+    }
 });
 
 //update post title
